ef3509d19ebf62689fd9c564cfd9a52106ef1e07
Aram Istamboulian <75444551+Aram-961@users.noreply.github.com>
2022-04-01 08:55:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("button")[0].addEventListener("click", function () {
+    document.querySelector("body").style.backgroundColor = "yellow";
+})
+
+document.querySelectorAll("button")[1].addEventListener("click", function () {
+    document.querySelector("body").style.backgroundColor = "white";
+})
